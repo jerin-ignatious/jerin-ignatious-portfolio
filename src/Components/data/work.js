dff2af37b4e8f99fb7ab2c2f4793b829ed3407bc
jerin-ignatious <jerinignatious1998@gmail.com>
2024-06-05 09:44:43 +0900

    changed work experience and added human resocia as current company

--- src/Components/data/work.js
+++ src/Components/data/work.js
@@ -1,11 +1,18 @@
 export const WorkData = [
     {
+        company: 'Human Resocia',
+        designation: 'Software Engineer',
+        dateJoining: 'May 2024',
+        companyLogo: 'https://gaijinpot-jobs.scdn3.secure.raxcdn.com/logo/37928/11813.large.png',
+        dateEnd: 'Present'
+    },
+    {
         company: 'Jupiter Money',
         designation: 'Software Development Engineer 1',
         dateJoining: 'Dec 2021',
-        dateEnd: 'Present',
-        companyLogo: 'https://downloadr2.apkmirror.com/wp-content/uploads/2023/06/81/649aadd99fa38_money.jupiter.png',
-        work: "Designed and developed an industry(Fintech) first solution which gave customers the option of using their ePAN instead of their Physical PAN card for completing video KYC. This helped customers with damaged or misplaced PAN cards to complete their video KYC.\n\nWorked in a team of 4 to build the user attribute change feature(email and phone number) which involved coordinating with multiple stakeholders and the partner bank.\n\nDesigned and implemented a feature(zeroState) containing multiple REST APIs which serve over 500k+ requests successfully on a daily basis.\n\nIdentified and fixed bugs which helped increase the user onboarding percentage from 66% to 80% and also reduced the user drop out rates faced during the onboarding flow.\n\nWorked in a two man team to migrate the majority of the partner bank API clients from feign to reactive using OpenAPI contracts which helped bring down the latencies of these APIs.\n\nPrimarily worked in kotlin and spring boot along with PostgreSQL, Kafka and various AWS provided services like SQS, DynamoDB, Lambda, S3 and API Gateway."
+        dateEnd: 'Apr 2024',
+        companyLogo: 'https://media.licdn.com/dms/image/D560BAQFSvwqeyMjrAQ/company-logo_200_200/0/1714668869358/jupiter_money_logo?e=2147483647&v=beta&t=VdYqGv8Fuof38nwJvfhDYW7rqXUdWvcSSz-ZY5_rGTc',
+        work: "Designed and developed an industry (Fintech) first solution which gave customers the option of using their ePAN instead of their Physical PAN card for completing video KYC. This helped customers with damaged or misplaced PAN cards to complete their video KYC.\n\nDeveloped a system consisting of multiple microservices which helped in creating a decoupled user onboarding flow for a multi-product environment. These microservices are handling over a million requests per week.\n\nDesigned and implemented a feature (zeroState) containing multiple REST APIs which serve over 500k+ requests successfully on a daily basis.\n\nIdentified and fixed bugs which helped increase the user onboarding percentage from 66% to 80% and also reduced the user drop out rates faced during the onboarding flow.\n\nWorked in a two man team to migrate the majority of the partner bank API clients from feign to reactive using OpenAPI contracts which helped bring down the latencies of these APIs.\n\nPrimarily worked in kotlin and spring boot along with PostgreSQL, Kafka and various AWS provided services like SQS, DynamoDB, Lambda, S3 and API Gateway."
     },
     {
         company: 'Tata Consultancy Services',
